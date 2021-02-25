4858221c9fa493da7ee6d72f7bbc53e09213dfe0
ecalderac <e.calderac@hotmail.com>
2021-02-24 21:01:46 -0300

    Finalizada seccion de componentes react

---
+++ aprendiendoreact/src/components/MiComponente.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class MiComponente extends Component {
+  render() {
+    let receta = {
+      nombre: "Pizza",
+      ingredientes: ["Tomate", "Queso", "Jamon"],
+      calorias: 400,
+    };
+    return (
+      <div className="miComponente">
+        <h1> {"Receta: " + receta.nombre} </h1>
+        <h2> {"Calorias: " + receta.calorias} </h2>
+        <ol>
+          {receta.ingredientes.map((ingrediente, i) => {
+            //console.log(ingrediente);
+            return <li key={i}>{ingrediente}</li>;
+          })}
+        </ol>
+      </div>
+      /*
+      <React.Fragment>
+        <h1> Hola, soy el componente llamado: MiComponente </h1>
+        <h2> Estoy probando el compoente </h2>
+      </React.Fragment>*/
+    );
+  }
+}
+
+export default MiComponente;
